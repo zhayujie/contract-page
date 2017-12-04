8d96d74b4192b1abf0f3ecdc3052e5702fbe1ea3
zyj <yjzha1996@163.com>
2017-12-04 20:20:23 +0800

    test

--- 6.c
+++ 6.c
@@ -1,41 +1,42 @@
+//的说法的生分啊
 #include <stdio.h>
 void sort(double * x, double * y, double * z);
 int main(void)
 {
-    double num1, num2 ,num3;   //现在用的是vc6.0++打的字
+	double num1, num2 ,num3;
 
-    printf("Enter 3 numbers: ");
-    scanf("%lf%lf%lf", &num1, &num2, &num3);
-    printf("Originally\nnum1 = %.2f, num2 = %.2f, num3 = %.2f\n",
-            num1, num2, num3);
-    sort(&num1, &num2, &num3);
-    printf("Now\nnum1 = %.2f, num2 = %.2f, num3 = %.2f\n",
-            num1, num2, num3);
+	printf("Enter 3 numbers: ");
+	scanf("%lf%lf%lf", &num1, &num2, &num3);
+	printf("Originally\nnum1 = %.2f, num2 = %.2f, num3 = %.2f\n",
+			num1, num2, num3);
+	sort(&num1, &num2, &num3);
+	printf("Now\nnum1 = %.2f, num2 = %.2f, num3 = %.2f\n",
+			num1, num2, num3);
 
-    return 0;
+	return 0;
 }
 
 void sort(double * x, double * y, double * z)
 {
-    double temp;
-    if (*y < *x)
-    {
-        temp = *y;
-        *y = *x;
-        *x = temp;
-    }
-    if (*z < *x)
-    {
-        temp = *z;
-        *z = *x;
-        *x = temp;
-    }
-    if (*z < *y)
-    {
-        temp = *z;
-        *z = *y;
-        *y = temp;
-    }
+	double temp;
+	if (*y < *x)
+	{
+		temp = *y;
+		*y = *x;
+		*x = temp;
+	}
+	if (*z < *x)
+	{
+		temp = *z;
+		*z = *x;
+		*x = temp;
+	}
+	if (*z < *y)
+	{
+		temp = *z;
+		*z = *y;
+		*y = temp;
+	}
 }
 
-        
+		
