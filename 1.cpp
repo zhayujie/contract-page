ee11e3878b080b5a7d24305bbb4427364087fdde
zyj <yjzha1996@163.com>
2017-12-04 15:09:49 +0800

    new file

---
+++ 1.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main(void) {
+    int a, b, c;
+    
+    for () {                    //123
+        for () {                //234
+            for () {            //345
+                adasd;          //22
+                asdad;          //21
+
+              //sadasdas
+
+            }
+            
+        }
+
+    }
+
+
+}
