570361580c06be8b02874d465b86c09f9261937b
zyj <yjzha1996@163.com>
2017-12-05 17:12:56 +0800

    test

--- 6.c
+++ 6.c
@@ -1,42 +1,10 @@
-//µÄËµ·¨µÄÉú·Ö°¡
-#include <stdio.h>
-void sort(double * x, double * y, double * z);
+#include<stdio.h>
 int main(void)
 {
-	double num1, num2 ,num3;
-
-	printf("Enter 3 numbers: ");
-	scanf("%lf%lf%lf", &num1, &num2, &num3);
-	printf("Originally\nnum1 = %.2f, num2 = %.2f, num3 = %.2f\n",
-			num1, num2, num3);
-	sort(&num1, &num2, &num3);
-	printf("Now\nnum1 = %.2f, num2 = %.2f, num3 = %.2f\n",
-			num1, num2, num3);
-
-	return 0;
+    int toes=10;
+    int toes_2,toes2;
+    toes_2=2*toes;
+    toes2=toes*toes;
+    printf("toesæ˜¯%d,toesçš„ä¸¤å€æ˜¯%d,toesçš„å¹³æ–¹æ˜¯%d\n",toes,toes_2,toes2);
+    return 0;
 }
-
-void sort(double * x, double * y, double * z)
-{
-	double temp;
-	if (*y < *x)
-	{
-		temp = *y;
-		*y = *x;
-		*x = temp;
-	}
-	if (*z < *x)
-	{
-		temp = *z;
-		*z = *x;
-		*x = temp;
-	}
-	if (*z < *y)
-	{
-		temp = *z;
-		*z = *y;
-		*y = temp;
-	}
-}
-
-		
