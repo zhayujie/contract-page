074d765ec8f71cd51e26b8993888679d46738227
zhayujie <yjzha1996@163.com>
2018-05-03 17:43:17 +0800

    feat: read images automatically

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
+# coding=utf8
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def hello():
-    return render_template('kun.html', n=range(1, 9)), 200
+	file_list = os.listdir('./static')
+	file_names = []
+	for i in range(0, len(file_list)):
+		file_name = file_list[i].split('.')[0]
+		if file_name:
+			file_names.append(file_name)
+	print(file_names)
+	return render_template('kun.html', file_names=file_names), 200
 
 if __name__ == '__main__':
     app.run(port=5000)
