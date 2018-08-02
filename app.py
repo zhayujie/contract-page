6a7569334b2180aee8c04f4c4c8674ee3cae7b66
zhayujie <yjzha1996@163.com>
2018-08-02 10:44:49 +0800

    添加了首页界面

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def signin_form():
-    return render_template('form.html'), 200
+    return render_template('index.html'), 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
