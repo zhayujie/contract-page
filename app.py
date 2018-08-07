37f1070b3e66c0f7ef4cb560bfe99a4931ee819a
zhayujie <yjzha1996@163.com>
2018-08-07 15:28:30 +0800

    完成了首页页面和登陆注册功能
     - 更换了首页背景
     - 添加了优势板块
     - 增加了条款前提下拉框

--- app.py
+++ app.py
@@ -1,10 +1,33 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
-def signin_form():
+def form():
     return render_template('index.html'), 200
 
+@app.route('/enroll', methods=['GET', 'POST'])
+def enroll():
+    if request.method == 'GET':
+        return render_template('enroll.html'), 200
+    else:
+        username = request.form.get('form-username', default='user')
+        password = request.form.get('form-password', default='pass')
+        print(username)
+        print(password)
+        return redirect('/')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html'), 200
+    else:
+        username = request.form.get('form-username', default='user')
+        password = request.form.get('form-password', default='pass')
+        print(username)
+        print(password)
+        return redirect('/')
+
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=80, debug=True)
